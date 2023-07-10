9f69b98dada7e2463cced25eb552597cfa34c407
BLAERx <132937420+BLAERx@users.noreply.github.com>
2023-07-10 19:45:41 +0300

    filters complete

--- kbs.py
+++ kbs.py
@@ -1,11 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-kb_client = ReplyKeyboardMarkup()
-b1 = KeyboardButton('Say 1')
-b2 = KeyboardButton('Say 2', request_contact=True)
-b3 = KeyboardButton('Say 3')
-
-kb_client.add(b1).add(b2)
-kb_client.row(b1, b2)
-kb_client.add(b1).insert(b3)
+def get_kb() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardMarkup( [
+        [KeyboardButton(row + column) for row in range(50)] for column in range(50)
+    ])
+    return kb
--- screen_bot.py
+++ screen_bot.py
@@ -4,14 +4,26 @@ import secrets
 from aiogram import Bot, Dispatcher, types, executor
 
 from tk import TOKEN_API
+from kbs import get_kb
 
 bot = Bot(token=TOKEN_API)
 dp = Dispatcher(bot=bot)
 
 
+@dp.message_handler(lambda msg: msg.text == 'hellow!', content_types=['text'])
+async def process_update(msg: types.Message) -> None:
+    await msg.answer(msg.from_user.first_name)
+    await msg.answer('Hello World')
+
+
 @dp.message_handler(commands=['start'])
 async def cmd_start(msg: types.Message) -> None:
-    await msg.answer('Text')
+    await msg.answer('Text', reply_markup=get_kb())
+
+
+@dp.message_handler(commands=['cancel'])
+async def cmd_cancel(msg: types.Message) -> None:
+    await msg.answer('Canceled', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(commands=['screen'])
@@ -24,6 +36,7 @@ async def cmd_screen(msg: types.Message) -> None:
     img.save(img_name, format='PNG')
 
     await msg.answer_photo(photo=open(img_name, 'rb'))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
